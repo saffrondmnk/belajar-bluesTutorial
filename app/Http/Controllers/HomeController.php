7c9ac7c03887d6880d6fa9fc489583d9d423b538
Saffron Damanik <saffrondmnk@gmail.com>
2024-02-02 08:26:31 +0700

    first time belajar

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function index()
+    {
+        $data = User::get();
+        return view('index', compact('data'));
+    }
+}
